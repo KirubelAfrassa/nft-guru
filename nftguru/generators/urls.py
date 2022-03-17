57caaf2341d6f806af06a7632531f939870ee1fa
Afrassa <kirawub.58@gmail.com>
2022-03-17 22:57:32 +0300

    feat: add preview url

--- nftguru/generators/urls.py
+++ nftguru/generators/urls.py
@@ -7,6 +7,7 @@ app_name = "generators"
 urlpatterns = [
     path("images/", views.LayerImageList.as_view()),
     path("images/<int:pk>/", views.ImageDetail.as_view()),
+    path("preview/", views.Preview.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
