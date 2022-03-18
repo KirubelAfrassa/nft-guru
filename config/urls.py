0a5227079988907df5be2e28caa39e66761ddb54
Afrassa <kirawub.58@gmail.com>
2022-03-19 00:37:03 +0300

    feat: add api/ as url root

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     # User management
     path("users/", include("nftguru.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
-    path("", include("nftguru.generators.urls")),
+    path("api/", include("nftguru.generators.urls")),
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- nftguru/generators/urls.py
+++ nftguru/generators/urls.py
@@ -5,9 +5,9 @@ from . import views
 
 app_name = "generators"
 urlpatterns = [
-    path("images/", views.LayerImageList.as_view()),
-    path("images/<int:pk>/", views.ImageDetail.as_view()),
-    path("preview/", views.Preview.as_view()),
+    path("images", views.LayerImageList.as_view()),
+    path("images/<int:pk>", views.ImageDetail.as_view()),
+    path("preview", views.Preview.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
