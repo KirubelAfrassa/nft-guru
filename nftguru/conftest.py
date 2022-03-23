9b8e2795ac84d4b1aa9ce04eb1426d460f8e6066
nurul <nurullahhcaliskan@gmail.com>
2022-03-23 17:10:42 +0300

    push test on new pc

--- nftguru/conftest.py
+++ nftguru/conftest.py
@@ -3,7 +3,6 @@ import pytest
 from nftguru.users.models import User
 from nftguru.users.tests.factories import UserFactory
 
-
 @pytest.fixture(autouse=True)
 def media_storage(settings, tmpdir):
     settings.MEDIA_ROOT = tmpdir.strpath
